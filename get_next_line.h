ddbc0834e7b82351009da9d77b55a5c79b722315
Murgueytio <108558181+Murgueytio@users.noreply.github.com>
2024-11-18 09:49:33 +0100

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_mix(char *str, char *buff);
+
+#endif
